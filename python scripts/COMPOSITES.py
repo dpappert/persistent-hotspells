10d60503b9bba470c2ea8d9ed9f5982339ec79ee
Duncan Pappert <duncan.pappert@unibe.ch>
2024-09-26 16:03:08 +0200

    Add files via upload

---
+++ python scripts/COMPOSITES.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env duncenv
+# coding: utf-8
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+import datetime as dt
+from datetime import timedelta
+from sklearn_extra.cluster import KMedoids
+from statsmodels.stats.multitest import fdrcorrection
+import multiprocessing as mp
+
+import HS_FUNCTIONS as mf  # ensure this file is in your current working directory
+
+
+path = '/specify path to your folder...'
+
+
+####################################################
+print('define long and short hotspells for all regions')
+####################################################
+
+# import excel table with hot spell durations/dates
+# same as output from python script HOTSPELLS.py
+hotspells = pd.read_excel(path + 'HS_dates.xlsx')
+
+long_spell_list = []
+for i in list(range(0, 12, 2)):
+    cl = hotspells.loc[hotspells.iloc[:, i].between(12, 26), hotspells.columns[i:i+2]]
+    long_spell_list.append(cl)
+    
+short_spell_list = []
+for i in list(range(0, 12, 2)):
+    cl = hotspells.loc[hotspells.iloc[:, i].between(4, 5), hotspells.columns[i:i+2]]
+    short_spell_list.append(cl)
+
+    
+####################################################
+print('define variable and parameters')
+####################################################
+
+var = xr.open_dataset(pth + 'filename.nc')['varname'] # import daily anomalies as xarray (time,lon,lat)
+cluster_index = 1 # specify which of the 6 clusters in 'hotspells' you want to work with
+varlab = var.name # string containing variable name, only important for file naming
+numMC = 1000 # number of Monte Carlo simulated hotspell sets for the significance test
+
+
+####################################################
+print('compute and save long/short spell composite')
+####################################################
+
+comp_l = hsf.cluster_composite(hsf.spell_means(hsf.extract_daily_spell_data(var, long_spell_list[cluster_index])))
+comp_l.to_netcdf(path + '/cl' + str(cluster_index+1) + '_' + varlab + '_long_comp.nc')
+comp_s = hsf.cluster_composite(hsf.spell_means(hsf.extract_daily_spell_data(var, short_spell_list[cluster_index])))
+comp_s.to_netcdf(path + '/cl' + str(cluster_index+1) + '_' + varlab + '_short_comp.nc')
+
+
+####################################################
+print('calculate significance for each spell-type composite and save mask')
+####################################################
+  
+simdates_l = hsf.get_N_sim_dates(long_spell_list[cluster_index])
+simdates_s = hsf.get_N_sim_dates(short_spell_list[cluster_index])
+
+for simdates, comp, spelltype in zip([simdates_l,simdates_s],[comp_l,comp_s],['long','short']):
+    def sim_comps_1000(sim):
+        simcomp = hsf.cluster_composite(
+            hsf.spell_means(
+                hsf.extract_daily_spell_data(var, pd.DataFrame([[np.nan] * len(simdates.iloc[sim]), simdates.iloc[sim]]).T)
+            )
+        )
+        return simcomp
+
+    pool = mp.Pool(100) # creates a pool of 100 processes that can run tasks in parallel
+    # but you should consider the capabilities of your hardware when deciding on the number of processes to use
+    simcomps1000 = pool.map(sim_comps_1000, range(numMC))  
+    pool.close()
+    pool.join()
+
+    # Calculate the rank and then adjusted p-value for the mth empirical field
+    x = comp
+    x_flat = x.values.flatten()[:, np.newaxis]
+    a = np.stack(simcomps1000, axis=0)
+    prod = x.shape[0] * x.shape[1]
+    a_flat = a.reshape((len(a), prod)).T
+    a_flat_sorted = np.sort(a_flat, axis=1)
+
+    rankMat = []
+    for i in range(len(x_flat)):
+        rank_val = np.searchsorted(a_flat_sorted[i], x_flat[i]) + 1
+        rankMat.append(int(rank_val))
+    rankMat = xr.DataArray(np.array(rankMat).reshape(x.shape[0],x.shape[1]), coords=x.coords)
+    pMat = (2*((numMC/2)-abs(rankMat-((numMC/2)+1)))/(numMC+1))
+    pMat_adj = hsf.apply_fdr(pMat, significance=.05, alpha=.1)
+    
+    pMat_adj.to_netcdf(path + '/cl' + str(cluster_index + 1) + '_' + varlab + '_' + spelltype + '_sign.nc')
+    print(spelltype + ' significance 2D mask saved')
+
---
+++ python scripts/HS_FUNCTIONS.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env duncenv
+# coding: utf-8
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+import datetime as dt
+from datetime import timedelta
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn_extra.cluster import KMedoids
+from statsmodels.stats.multitest import fdrcorrection
+
+
+
+def expand_date_range(date_range):
+    start, end = date_range.split('/')
+    start_date = pd.to_datetime(start)
+    end_date = pd.to_datetime(end)
+    date_range = pd.date_range(start=start_date, end=end_date)
+    return date_range
+
+
+def remove_first_last_two_dates(datetime_index):
+    return datetime_index[2:-2]
+
+
+def subsample_long_spells_emp(long_spell_list, cluster_index, deterministic=True, shuffle=True):
+    long_spell_list_subsampled = []
+
+    long_spells_days = long_spell_list[cluster_index].iloc[:,1].apply(expand_date_range).reset_index(drop=True)
+    if deterministic and not shuffle:
+        weights = np.ones(len(long_spell_list[cluster_index]))/len(long_spell_list[cluster_index])
+        indices = np.random.choice(len(long_spell_list[cluster_index]), size=len(short_spell_list[cluster_index]), p=weights)
+        selected_sets = np.array(long_spells_days)[indices]
+        centre_dates = np.array([np.random.choice(datetime_set).astype('datetime64[D]') for datetime_set in selected_sets])
+    elif not deterministic and not shuffle:
+        all_dates = np.concatenate(long_spells_days).astype('datetime64[D]')
+        centre_dates = np.random.choice(all_dates, len(short_spell_list[cluster_index]))
+    elif not deterministic and shuffle:
+        all_dates = np.concatenate(long_spells_days).astype('datetime64[D]')
+        np.random.shuffle(all_dates)
+        centre_dates = np.random.choice(all_dates, len(short_spell_list[cluster_index]))
+
+    num_4day_spells = len(short_spell_list[cluster_index][short_spell_list[cluster_index].iloc[:,0] == 4])
+    num_5day_spells = len(short_spell_list[cluster_index][short_spell_list[cluster_index].iloc[:,0] == 5])
+    long_5days_expanded = []
+    for date in centre_dates:
+        if np.random.random() < (num_4day_spells / len(short_spell_list[cluster_index])):
+            spell = pd.date_range(date-2,date+1)
+        else:
+            spell = pd.date_range(date-2,date+2)
+        long_5days_expanded.append(spell)
+    long_5days_expanded = pd.Series(long_5days_expanded)
+    long_5days_ranges = long_5days_expanded.apply(lambda datetime_index: 
+                        f"{datetime_index[0].strftime('%Y-%m-%d')}/{datetime_index[-1].strftime('%Y-%m-%d')}")
+    long_spell_list_subsampled = pd.DataFrame([[len(spell) for spell in long_5days_expanded], long_5days_ranges]).T
+    return long_spell_list_subsampled
+
+
+def extract_daily_spell_data(dataset, spell_list):
+    subsets = []
+    for i in range(0, len(spell_list)):
+        subset = dataset.sel(time=slice(pd.to_datetime(spell_list.iloc[:, 1].str.split('/').str[0], format='%Y-%m-%d').iloc[i],
+                        pd.to_datetime(spell_list.iloc[:, 1].str.split('/').str[1], format='%Y-%m-%d').iloc[i]+timedelta(1)))
+        subsets.append(subset)
+    return subsets
+
+
+def spell_means(subsets):
+    spell_means = [df.mean(dim='time') for df in subsets]
+    return spell_means
+
+
+def cluster_composite(spell_means):
+    composite = xr.concat(spell_means, dim='time').mean(dim='time')
+    return composite
+
+
+def medoid100(composites):
+    data = []
+    for ds in composites:
+        flattened = ds[1].values.flatten()
+        # Remove NaN values before appending
+        flattened = flattened[~np.isnan(flattened)]
+        data.append(flattened)
+    n_clusters = 1
+    kmedoids = KMedoids(n_clusters=n_clusters, random_state=0)
+    kmedoids.fit(data)
+    medoid_index = kmedoids.medoid_indices_[0]
+    return medoid_index
+
+
+def get_N_sim_dates(spell_list_cl):
+    
+    doyVec = pd.to_datetime(spell_list_cl.iloc[:, 1].str.split('/').str[0], format='%Y-%m-%d').dt.dayofyear # doy for start of spell
+    pdf_doy = sns.kdeplot(doyVec, color='black', alpha=0.6, bw_adjust=.3, cut=1.5, linewidth=3); plt.close()
+    doyVec_pdf = pd.DataFrame(pdf_doy.lines[0].get_xydata(), columns=['doyVec','p'])
+    doyVec_pdf['doyVec'] = round(doyVec_pdf['doyVec'], 0)
+    doyVec_pdf = doyVec_pdf.groupby('doyVec').mean()
+    durations = spell_list_cl.iloc[:, 0]
+    pdf_duration = sns.kdeplot(durations, color='red', alpha=0.6, bw_adjust=.3, cut=0, linewidth=3); plt.close()
+    durations_pdf = pd.DataFrame(pdf_duration.lines[0].get_xydata(), columns=['duration','p'])
+    durations_pdf['duration'] = round(durations_pdf['duration'], 0)
+    durations_pdf = durations_pdf.groupby('duration').mean()
+
+    sim_doyVec = []
+    numMC = 1000
+    for n in range(numMC):
+        samples = []
+        for i in range(len(doyVec)):
+            random_doyVec = np.random.choice(doyVec_pdf.index, p=(doyVec_pdf['p'] + (( 1 - doyVec_pdf['p'].sum()) / len(doyVec_pdf['p']))) )
+            samples.append(random_doyVec)
+        sim_doyVec.append(samples)
+    sim_doyVec = pd.DataFrame(sim_doyVec)
+    years = pd.DataFrame(np.random.randint(1959,2023,size=(numMC, len(doyVec))))
+    sim_dates = []
+    for r in range(numMC):
+        for c in range(len(doyVec)):
+            sim_dates.append(dt.datetime.strptime(str(years[c][r]) + ' ' + str(int(sim_doyVec[c][r])), '%Y %j').strftime('%Y-%m-%d'))
+    sim_dates = pd.DataFrame(np.array(sim_dates).reshape(numMC,len(doyVec)))
+    end_dates = sim_dates.applymap(lambda x: pd.to_datetime(x) + pd.Timedelta(days=np.random.choice(durations_pdf.index,
+                        p=(durations_pdf['p'] + (( 1 - durations_pdf['p'].sum()) / len(durations_pdf['p']))))) )
+    end_dates = end_dates.applymap(lambda x: x.strftime('%Y-%m-%d')).astype('object')
+    sim_date_ranges = sim_dates.applymap(str) + '/' + end_dates.applymap(str)
+    
+    return sim_date_ranges
+
+
+def apply_fdr(pMat, significance, alpha):
+    
+    p2D = pMat.to_dataframe('p-value')
+    p2D_sorted = p2D.sort_values(p2D.columns[0], ascending = True)
+    fdr = fdrcorrection(p2D_sorted['p-value'], alpha=alpha, method='indep', is_sorted=True)
+    p2D_sorted['p-adjusted'] = fdr[1]
+    pMat_adj = p2D_sorted['p-adjusted'].to_xarray()
+    pMat_adj.values[pMat_adj >= significance] = np.nan
+    pMat_adj.values[pMat_adj < significance] = 1
+    
+    return pMat_adj
