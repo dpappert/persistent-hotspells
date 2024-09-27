fd68e6ce27ca7f441f3256be346110110efda6bb
Duncan Pappert <duncan.pappert@unibe.ch>
2024-09-27 13:38:34 +0200

    Update 032_COMPOSITES.py

--- python scripts/032_COMPOSITES.py
+++ python scripts/032_COMPOSITES.py
@@ -6,7 +6,6 @@ import pandas as pd
 import xarray as xr
 import datetime as dt
 from datetime import timedelta
-from sklearn_extra.cluster import KMedoids
 from statsmodels.stats.multitest import fdrcorrection
 import multiprocessing as mp
 
